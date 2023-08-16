b3189d0fba7bcbfe8f6a34922ef45d0c9e459be6
Shawn Huelle <shuelle@posteo.de>
2023-08-16 14:52:49 +0200

    create basic card scaffolding with html in card.js

--- components/card/card.js
+++ components/card/card.js
@@ -1,1 +1,27 @@
-export function createCharacterCard() {}
+const cardContainer = document.querySelector(".card-container");
+
+export function createCharacterCard() {
+  const listItem = `
+    <li class="card">
+    <div class="card__image-container">
+    <img
+      class="card__image"
+      src="#"
+      alt="#"
+    />
+    <div class="card__image-gradient"></div>
+  </div>
+  <div class="card__content">
+    <h2 class="card__title">#</h2>
+    <dl class="card__info">
+      <dt class="card__info-title">Status</dt>
+      <dd class="card__info-description">#</dd>
+      <dt class="card__info-title">Type</dt>
+      <dd class="card__info-description">#</dd>
+      <dt class="card__info-title">Occurrences</dt>
+      <dd class="card__info-description">#</dd>
+    </dl>
+  </div>
+    </li>
+  `;
+}
